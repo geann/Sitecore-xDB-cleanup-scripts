4084365890fb7bbf17a895007955d63c468a81bd
Anna Gevel <anna.gevel@deleteagency.com>
2023-03-29 07:54:51 +0100

    added comments

--- scripts/EXM Interactions.sql
+++ scripts/EXM Interactions.sql
@@ -1,7 +1,6 @@
 SELECT InteractionId INTO TempInteractions FROM [xdb_collection].[Interactions] 
-	WHERE 
-	UserAgent = 'Sitecore/9.0 (EXM)'
-	AND CampaignId = '0F5C6F03-0682-4279-BD60-B06A399E8824'
+	WHERE UserAgent = 'Sitecore/9.0 (EXM)' --replace with another user agent if using a different version of Sitecore
+	AND CampaignId = '0F5C6F03-0682-4279-BD60-B06A399E8824' -- replace with your campaign ID or remove this condition
 	AND StartDateTime < '2022-09-01'
 
 SELECT * FROM TempInteractions
