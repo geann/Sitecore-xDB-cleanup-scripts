920880596fe8930c7686db0df29026ad7f235138
Anna Gevel <anngevel@gmail.com>
2023-09-06 09:03:57 +0100

    Added a script for old interactions

---
+++ scripts/Old interactions.sql
@@ -0,0 +1,16 @@
+SELECT InteractionId INTO TempInteractions FROM [xdb_collection].[Interactions] 
+	AND StartDateTime < '2022-09-01'
+
+SELECT * FROM TempInteractions
+GO 
+
+DELETE FROM [xdb_collection].[InteractionFacets] WHERE InteractionId IN 
+	(SELECT InteractionId FROM TempInteractions)
+GO
+
+DELETE FROM [xdb_collection].[Interactions] WHERE InteractionId IN 
+	(SELECT InteractionId FROM TempInteractions)
+GO
+
+DROP TABLE TempInteractions
+GO
